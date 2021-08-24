a6276c6f3c36434bb21aaa260a54f5523b32ab37
rpiotrow <rpiotrow@gmail.com>
2021-08-24 15:25:53 +0200

    setup.py config

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='LOTR-Lector',
-      version='1.0',
-      description='LOTR-Lector for LOTR: Journeys in Middle-Earth App',
-      author='Rafal Piotrowski',
-      author_email='rpiotrow96@gmail.com',
-      packages=['gtts', 'playsound==1.2.2', 'pytesseract', 'opencv-python', 'pywin32'],
-      )
+setuptools.setup(name='LOTR-Lector',
+                 version='1.0',
+                 description='LOTR-Lector for LOTR: Journeys in Middle-Earth App',
+                 author='Rafal Piotrowski',
+                 author_email='rpiotrow96@gmail.com',
+                 install_requires=['gtts', 'playsound==1.2.2', 'pytesseract', 'opencv-python', 'pywin32'],
+                 )
